fcee3288769a195d12872ddeff5a6f2ad7f2fe95
lim-ad <lim-ad@glodon.com>
2020-04-21 10:02:18 +0800

    生成jar包

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,16 @@
+package controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class Controller {
+    @RequestMapping(value = "/jenkins", method = RequestMethod.GET)
+    public void viewBusiness() {
+        System.out.println("hello world");
+    }
+}
